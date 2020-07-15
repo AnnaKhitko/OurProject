38ea79740620cec33cdc2f1afac82d37ac077ed6
Dorra <dorra.hbaieb@gmail.com>
2020-07-15 14:30:07 +0200

    navbar v0
    navbar v0

---
+++ navbar.php
@@ -0,0 +1,16 @@
+<nav>
+    <ul>
+        <li>
+            <a href="home.php">Home</a>
+        </li>
+        <li>
+            <a href="movies.php">Movies</a>
+        </li>
+        <li>
+            <a href="category.php">Category</a>
+        </li>
+        <li>
+            <a href="add_movie.php">Add movie</a>
+        </li>
+    </ul>
+</nav>
