89ec75bde9874148562b4dfea6861baed7e0139c
AnnaKhitko <33467618+AnnaKhitko@users.noreply.github.com>
2020-07-15 15:17:28 +0200

    final reg page

--- register.php
+++ register.php
@@ -9,54 +9,59 @@
 </head>
 
 <body>
-    <!-- <?php require_once 'navbar.php'; ?> -->
-    <h1>Register to the website</h1>
+    <h1>Best movies</h1>
+    <hr>
+    <h3>Register to the website</h3>
     <br>
 
     <?php
-    $mail = '';
     //array for errors
     $errors = array();
 
     // Check if the form was submitted
     if (isset($_POST['submit'])) {
-        $firstName = $_POST['firstname'];
-        $lastName = $_POST['lastname'];
-        $email = $_POST['mail'];
+        $userName = $_POST['username'];
+        $email = $_POST['email'];
         $password = $_POST['password'];
         // First, I clean the email
         $sanitizeMail = filter_var($email, FILTER_SANITIZE_EMAIL);
         // Verify the format
         $sanitizeMail = filter_var($sanitizeMail, FILTER_VALIDATE_EMAIL);
 
+        //first name not empty
+        if (empty($userName))
+            $errors['username'] = 'Name is mandatory.';
         //if the email is valid
         if (!$sanitizeMail)
             $errors['mail'] = 'You must enter a valid email address.';
         //not empty password
         if (empty($password))
             $errors['password'] = 'Password is mandatory.';
-        //first name not empty
-        if (empty($firstName))
-            $errors['firstname'] = 'First name is mandatory.';
+        //password length
+        if ((strlen($password) < 8)) {
+            $errors['passwordLength'] = "Wrong password length. It should be more than 8 characters.";
+        }
+        //password confirmation
+        if ($_POST['password'] != $_POST['repeatPass']) {
+            $errors['passwordConfirmation'] =  "Your password and confirmation password do not match.";
+        }
 
-        if (empty($lastName))
-            $errors['lastname'] = 'Last name is mandatory.';
 
         // If there is no errors, insert user into DB
         if (count($errors) == 0) {
 
             //connect to the DB
             require_once 'database.php';
-            $conn = mysqli_connect(DB_SERVER, DB_USER, DB_PASSWORD, 'spotify', '8889');
-            //$conn = mysqli_connect('localhost', 'root', 'root', 'spotify', '8889');
+            $conn = mysqli_connect(DB_SERVER, DB_USER, DB_PASSWORD, 'movieProject', '8889');
 
             // Make sure email is not already taken
             $selectQuery = "SELECT *
-            FROM USERS
-            WHERE mail ='{$sanitizeMail}'";
+            FROM users
+            WHERE email ='$sanitizeMail'";
 
             $result_query = mysqli_query($conn, $selectQuery);
             $count = mysqli_num_rows($result_query);
+
 
             if ($count > 0) {
                 // EMAIL ALREADY TAKEN !
@@ -67,8 +72,8 @@
                 $securePassword = password_hash($password, PASSWORD_DEFAULT);
 
                 // Prepare & Execute query
-                $query = "INSERT INTO USERS(first_name, last_name, mail, password)
-                VALUES('$firstName','$lastName', '$sanitizeMail', '$securePassword')";
+                $query = "INSERT INTO users(name,password, email)
+                VALUES('$userName','$securePassword','$sanitizeMail')";
                 // var_dump($query);
 
                 $result_query = mysqli_query($conn, $query);
@@ -84,14 +89,23 @@
             }
             mysqli_close($conn);
         }
+        //show the errors
+        foreach ($errors as $error) {
+            echo $error . ' <br>';
+        }
+        if (count($errors) == 0) {
+            echo ' ';
+        }
     }
     ?>
 
     <form action="" method="post">
-        <input type="text" name="firstname" placeholder="First name"><br>
-        <input type="text" name="lastname" placeholder="Last name"><br>
-        <input type="text" name="mail" placeholder="Email"><br>
+        <input type="text" name="username" placeholder="Name"><br>
+        <input type="text" name="email" placeholder="Email"><br>
         <input type="password" name="password" placeholder="Password"><br>
+        <input type="password" name="repeatPass" placeholder="Repeat password"><br>
+
+        <!-- <input type="password" name="passwordCheck" placeholder="Repeat password"><br> -->
         <input type="submit" name="submit" value="Register">
     </form>
 </body>
