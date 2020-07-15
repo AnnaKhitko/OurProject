6a128bc0ba3a9b2cc44b1fa4eb98a2163ced54ed
Dorra <dorra.hbaieb@gmail.com>
2020-07-15 16:03:21 +0200

    v0 login
    FORM WORKS

---
+++ login.php
@@ -0,0 +1,62 @@
+<?php
+
+$errors = array();
+$error ='';
+
+if (isset($_POST['submit'])){
+    $login = false;
+    $email = htmlspecialchars($_POST["email"]);
+    $password = htmlspecialchars($_POST["password"]);
+
+    $sanitizeMail = filter_var($email, FILTER_SANITIZE_EMAIL);
+    $filterdMail = filter_var($sanitizeMail, FILTER_VALIDATE_EMAIL);
+    
+    if(!$filterdMail){
+        $errors[] = "Email is mandatory ! ";
+    }
+
+    if(empty($password)){
+        $errors[] = "password is mandatory !";
+    }
+    if(count($errors) == 0){
+        include_once 'database.php';
+        $connection = mysqli_connect(DB_SERVER, DB_USER, DB_PASSWORD, 'movieProject', '8889');
+        $query = "SELECT * 
+        FROM users 
+        WHERE email ='".$filterdMail."'";
+        $result_query = mysqli_query($connection, $query);
+        $user = mysqli_fetch_assoc($result_query);
+        
+        if(!empty($user)){
+            //$passwordVerified = password_verify($password, $user['password']);
+            if($password){
+                //header('Location:home.php');
+                $login = true;
+                $_SESSION['email'] = $filterdMail;
+                $_SESSION['username'] = $user['name'];
+                $_SESSION['id'] = $user["userId"];
+                
+            }
+        }
+    }
+    else{
+        $error = implode('<br>', $errors);
+    }
+    if($login){
+        echo 'welcome ! ';
+    }else{
+        echo 'try again';
+    }
+}
+
+?>
+
+<form action="" method="POST">
+<label for="email">Email address</label>
+<input type="text" id="email" name="email">
+<label for="password">Password</label>
+<input type="text" id="password" name="password">
+<input type="submit" name="submit" value="Login">
+<p><a href="register.php">Register</a></p>
+</form>
+<span><?= $error ?></span>
