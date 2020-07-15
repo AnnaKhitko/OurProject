01e662dd85c4653bf1b3bca85a23ae6d552a2d73
AnnaKhitko <33467618+AnnaKhitko@users.noreply.github.com>
2020-07-15 20:13:48 +0200

    show the movies info

--- catalogue/catalogue.php
+++ catalogue/catalogue.php
@@ -9,6 +9,39 @@
 </head>
 
 <body>
+    <?php require_once 'navbar.php'; ?>
+    <h2>All the movies</h2>
+    <?php
+
+    //connect to the DB
+    require_once 'database.php';
+    $conn = mysqli_connect(DB_SERVER, DB_USER, DB_PASSWORD, 'movieProject', '8889');
+
+    //$orderBy = 'm.movieId';
+
+    if ($conn) {
+
+        $query = "SELECT * FROM movies";
+
+        $result = mysqli_query($conn, $query);
+
+        if ($result) {
+            $movies = mysqli_fetch_all($result, MYSQLI_ASSOC);
+            // echo '<pre>';
+            // var_dump($movies);
+            // echo '</pre>';
+
+            foreach ($movies as $movie) {
+                echo '<img  height = 200px src="' . $movie['poster'] . '">' . '<br>';
+                echo '#' . $movie['movieId'];
+                echo $movie['title'] . '<br>';
+                echo substr($movie['synopsis'], 0, 30) . '...' . '<br>' . '<a href="http://localhost:8888/PHP/DB_ex/Movie_Ex_4.php?movieId=' . $movie['movieId'] . '">more</a><br><hr>';
+            }
+        }
+    } else {
+        echo 'Problems with connestion';
+    };
+    ?>
 
 </body>
 
