9c5dc5fded2e32da8f6f495735d04c99762553d5
AnnaKhitko <33467618+AnnaKhitko@users.noreply.github.com>
2020-07-15 21:36:46 +0200

    pagination and sorting

--- catalogue/catalogue.php
+++ catalogue/catalogue.php
@@ -12,16 +12,51 @@
     <?php require_once 'navbar.php'; ?>
     <h2>All the movies</h2>
     <?php
+    $getParams = $_GET;
+    $getParams['order'] = 'desc';
+    $orderDesckLink = http_build_query($getParams);
+
+    $getParams['order'] = 'asc';
+    $orderAscLink = http_build_query($getParams);
+    ?>
+    <a href="?<?= $orderDesckLink; ?>">Never movies firs</a>
+    <a href="?<?= $orderAscLink; ?>">Older movies first</a>
+    <br>
+    <br>
+    <?php
 
     //connect to the DB
     require_once 'database.php';
     $conn = mysqli_connect(DB_SERVER, DB_USER, DB_PASSWORD, 'movieProject', '8889');
 
-    //$orderBy = 'm.movieId';
+    $orderBy = 'movieId';
+    $orderType = 'ASC';
+    if (isset($_GET['order'])) {
+        $orderBy = 'year';
+        $orderType = $_GET['order'];
+    }
+
+    $page = 1;
+    if (isset($_GET['page'])) {
+        $page = $_GET['page'];
+    }
+
+    // = 
+    // $page = isset($_GET['page']) ? $_GET['page'] : 1;
+    // $page = $_GET['page'] ?? 1;
 
     if ($conn) {
+        $limit = 3;
+        $offset = $limit * ($page - 1);
 
-        $query = "SELECT * FROM movies";
+        $countQuery = "SELECT count(*) as total FROM movies";
+        $countResult = mysqli_query($conn, $countQuery);
+        $count = mysqli_fetch_assoc($countResult);
+        $count = (int) $count['total'];
+
+        $query = "SELECT * FROM movies
+        ORDER BY $orderBy $orderType
+        LIMIT $limit OFFSET $offset";
 
         $result = mysqli_query($conn, $query);
 
@@ -37,6 +72,37 @@
                 echo $movie['title'] . '<br>';
                 echo substr($movie['synopsis'], 0, 30) . '...' . '<br>' . '<a href="http://localhost:8888/PHP/DB_ex/Movie_Ex_4.php?movieId=' . $movie['movieId'] . '">more</a><br><hr>';
             }
+
+            $paginationLinks = [];
+            $getParams = $_GET;
+            if ($page > 1) {
+                $getParams['page'] = $page - 1;
+                $paginationLinks[] = [
+                    'title' => 'Previous',
+                    'link' => '?' . http_build_query($getParams),
+                ];
+            }
+
+            $getParams['page'] = $page;
+            $paginationLinks[] = [
+                'title' => $page,
+                'link' => '?' . http_build_query($getParams)
+            ];
+            if ($page * $limit < $count) {
+                $getParams['page'] = $page + 1;
+                $paginationLinks[] = [
+                    'title' => 'Next',
+                    'link' => '?' . http_build_query($getParams)
+                ];
+            }
+
+    ?>
+            <div>
+                <?php foreach ($paginationLinks as $link) { ?>
+                    <a href="<?= $link['link']; ?>"><?= $link['title']; ?></a>
+                <?php } ?>
+            </div>
+    <?php
         }
     } else {
         echo 'Problems with connestion';
