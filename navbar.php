e04f04a87c4f5c192d82999787b7c3ee8fb0c2d9
AnnaKhitko <33467618+AnnaKhitko@users.noreply.github.com>
2020-07-16 12:46:41 +0200

    new navbar

--- navbar.php
+++ navbar.php
@@ -7,19 +7,3 @@
         echo '<span> |  <a href="./add_movie.php">Add movie</a></span>';
     } ?>
 </nav>
-<!-- <nav>
-    <ul>
-        <li>
-            <a href="./home.php">Home</a>
-        </li>
-        <li>
-            <a href="./movies.php">Movies</a>
-        </li>
-        <li>
-            <a href="./category.php">Category</a>
-        </li>
-        <li>
-            <a href="./add_movie.php">Add movie</a>
-        </li>
-    </ul>
-</nav> -->
